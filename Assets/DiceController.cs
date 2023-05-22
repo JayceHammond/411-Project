cd3e4cabb2bb33a3c128f0fe2f677755a434ee62
JayceHammond <hjayce6@gmail.com>
2023-05-22 02:20:42 -0500

    Dice Roller
    forgot to commit this 2 days ago

---
+++ Assets/DiceController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DiceController : MonoBehaviour
+{
+    public GameObject twentySided;
+    public float rollSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K)){
+            GameObject roll = Instantiate(twentySided, this.transform);
+            roll.GetComponent<Rigidbody>().velocity = new Vector3(roll.GetComponent<Rigidbody>().velocity.x, roll.GetComponent<Rigidbody>().velocity.y, roll.GetComponent<Rigidbody>().velocity.z + Random.Range(20, 40));
+            Destroy(roll, 5);
+        }
+    }
+}
