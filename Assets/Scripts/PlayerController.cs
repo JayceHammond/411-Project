321aab9983f3422d432792c170c84ec01c2876c8
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2024-01-20 11:05:44 -0600

    Have Conditionals on all movement options
    Only Soilder

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float playerSpeed = 2.0f;
     [SerializeField]
-    private float jumpHeight = 1.0f;
+    private float jumpHeight = 3.0f;
     [SerializeField]
     private float gravityValue = -9.81f;
     private CharacterController controller;
@@ -25,31 +25,36 @@ public class PlayerController : MonoBehaviour
         inputManager = InputManager.Instance;
         cameraTransform = Camera.main.transform;
 
+        //Need this to trigger the animations
         animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
         }
         
+        //Vectors that grab the movement from the player's input to move the char
         Vector2 movement = inputManager.GetPlayerMovement();
         Vector3 move = new Vector3(movement.x, 0f, movement.y);
-        move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
-        move.y = 0f;
-        controller.Move(move * GameTime.deltaTime * playerSpeed);
-        playerVelocity.y += gravityValue * GameTime.deltaTime;
-        controller.Move(playerVelocity * GameTime.deltaTime);
-
+       
+       //Conditonals for walking to try to keep the movement and the animations in the same place
         if(math.abs(movement.x) > 0 || math.abs(movement.y) > 0){
 
             animator.ResetTrigger("Idle");
             animator.SetTrigger("Walk");
 
+            move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
+            controller.Move(move * GameTime.deltaTime * playerSpeed);
+            
+            move.y = 0f;
+            
+            //To get proper oritation? of the character based on where they are walking
             if(movement.x < 0 && controller.transform.localRotation.eulerAngles.y == 0){
                 controller.transform.Rotate(0f, 180f, 0f, Space.Self);
             }else if(movement.x > 0 && controller.transform.localRotation.eulerAngles.y == 180){
@@ -61,7 +66,14 @@ public class PlayerController : MonoBehaviour
             animator.SetTrigger("Idle");
         }
 
+        //Conditonals for Jump movement and Animation
         if(movement.y > 0){
+
+            //playerVelocity.y = jumpHeight;
+
+            playerVelocity.y += gravityValue * GameTime.deltaTime;
+            controller.Move(playerVelocity * GameTime.deltaTime);
+
             animator.SetTrigger("Jumping");
         }
     }
