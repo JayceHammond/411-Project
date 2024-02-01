63c3795121f4ad79147a5e4aa4505948aea185ce
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2024-02-01 11:06:32 -0600

    Remade the DM Camera Folder
    Its in DM Scripts just so the DM scripts are more organized.

--- Assets/Scripts/DM_Scrpits/DM Camera/DM_Camera_Controller.cs
+++ Assets/Scripts/DM_Scrpits/DM Camera/DM_Camera_Controller.cs
@@ -14,7 +14,7 @@ public class DMCameraController : MonoBehaviour
     private float sensitivity = 1.0f;
 
     private MouseInputs mouseInputs;
-    private Camera camera;
+    private new Camera camera;
     
     // Start is called before the first frame update
     void Start()
