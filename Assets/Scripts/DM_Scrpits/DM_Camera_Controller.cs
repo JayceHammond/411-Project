4e5a5a0a2a283f75a8e5e23db4733d4e93887293
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2024-02-01 11:03:08 -0600

    Camera Controls done
    All the camera controls should be done

--- Assets/Scripts/DM_Scrpits/DM_Camera_Controller.cs
+++ Assets/Scripts/DM_Scrpits/DM_Camera_Controller.cs
@@ -14,44 +14,36 @@ public class DMCameraController : MonoBehaviour
     private float sensitivity = 1.0f;
 
     private MouseInputs mouseInputs;
+    private Camera camera;
     
     // Start is called before the first frame update
     void Start()
     {
         mouseInputs = MouseInputs.Instance;
+        camera = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Sensitivity is a slider that allows the user to adjust the movement speed of their camera to move faster or slower
-        sensitivity += mouseInputs.scrollWheel().y;
-        if(sensitivity < 0 ){
-            //Makes moves slower
-            sensitivity = math.abs(sensitivity * 0.25f);
-        }else if(sensitivity >= 1) {
-            //Changes from fractional movement to whole numbers
-            sensitivity = (int)sensitivity;
-        }else if(sensitivity >= 10){
-            //Limits the max movement speed to 10 since going above seems excessive
-            sensitivity = 10;
-        }
-
-        //Move the camera based on mouse movement while pressing MiddleMouse Button
+        // Move the camera based on mouse movement while pressing MiddleMouse Button
         if (mouseInputs.scrollWheelClick())
         {
-            //Locks the cursor moving the camera
+            // Locks the cursor moving the camera
             mouseInputs.isFocused(true);
             transform.position += new Vector3(mouseInputs.MouseMovement().x  * sensitivity, -mouseInputs.MouseMovement().y * sensitivity, 0);
         }else{
             mouseInputs.isFocused(false);
         }
-        
 
         // Rotate the camera based on the mouse movement while pressing RightClick
         if (mouseInputs.rightClick())
         {
             transform.eulerAngles += new Vector3(mouseInputs.MouseMovement().y, mouseInputs.MouseMovement().x, 0);
+            camera.fieldOfView += mouseInputs.scrollWheel().y;
+        }else{
+            // Scrollwheel moves the camera in the Z direction
+            transform.position += new Vector3(0 , 0 , -mouseInputs.scrollWheel().y);
         }
 
     }
