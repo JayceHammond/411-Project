7f6ccfd25c88d6024a1b18b03e81df9c9b89b8cb
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2024-02-01 10:49:42 -0600

    Changed to unity editor camera controls
    The only on not changed is the scrollwheel zoom

--- Assets/Scripts/DM_Scrpits/DM_Camera_Controller.cs
+++ Assets/Scripts/DM_Scrpits/DM_Camera_Controller.cs
@@ -13,25 +13,22 @@ public class DMCameraController : MonoBehaviour
     [SerializeField]
     private float sensitivity = 1.0f;
 
-    private float newSpeed;
-    private float rotateX;
-    private float rotateY;
-
+    private MouseInputs mouseInputs;
+    
     // Start is called before the first frame update
     void Start()
     {
-    
+        mouseInputs = MouseInputs.Instance;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Sensitivity is a slider that allows the user to adjust the movement speed of their camera to move faster or slower
-        sensitivity += Input.mouseScrollDelta.y;
+        sensitivity += mouseInputs.scrollWheel().y;
         if(sensitivity < 0 ){
             //Makes moves slower
             sensitivity = math.abs(sensitivity * 0.25f);
-            Debug.Log(sensitivity);
         }else if(sensitivity >= 1) {
             //Changes from fractional movement to whole numbers
             sensitivity = (int)sensitivity;
@@ -40,33 +37,22 @@ public class DMCameraController : MonoBehaviour
             sensitivity = 10;
         }
 
-        // Move the camera forward, backward, left, and right
-        transform.position += transform.forward * Input.GetAxis("Vertical") * intialSpeed * sensitivity * Time.deltaTime;
-        transform.position += transform.right * Input.GetAxis("Horizontal") * intialSpeed * sensitivity * Time.deltaTime;
-
-        if (Input.GetKey(KeyCode.Z))
+        //Move the camera based on mouse movement while pressing MiddleMouse Button
+        if (mouseInputs.scrollWheelClick())
         {
-            transform.position += transform.up;
-        }
-        else if(Input.GetKey(KeyCode.X)){
-            transform.position -= transform.up;
-        }
-
-        // Rotate the camera based on the mouse movement
-        if (Input.GetKey(KeyCode.Q))
-        {
-            rotateX--;
-            transform.eulerAngles += new Vector3(0, rotateX, 0);
-        }
-        else if(Input.GetKey(KeyCode.E)){
-            rotateX++;
-            transform.eulerAngles += new Vector3(0, rotateX, 0);
+            //Locks the cursor moving the camera
+            mouseInputs.isFocused(true);
+            transform.position += new Vector3(mouseInputs.MouseMovement().x  * sensitivity, -mouseInputs.MouseMovement().y * sensitivity, 0);
         }else{
-            rotateX = 0;
+            mouseInputs.isFocused(false);
         }
-
-        rotateY = Input.GetAxis("Mouse Y");
         
+
+        // Rotate the camera based on the mouse movement while pressing RightClick
+        if (mouseInputs.rightClick())
+        {
+            transform.eulerAngles += new Vector3(mouseInputs.MouseMovement().y, mouseInputs.MouseMovement().x, 0);
+        }
 
     }
 }
