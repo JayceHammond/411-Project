251db1db2ada3c5cbfb53f9cc452d1ebbd08892c
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2024-03-04 12:17:49 -0600

    Got Drag working with Selection

--- Assets/Scripts/DM_Scripts/DM_Menus/Not Used but Considered/DragScript.cs
+++ Assets/Scripts/DM_Scripts/DM_Menus/Not Used but Considered/DragScript.cs
@@ -1,26 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DragScript : MonoBehaviour
+public class DragScript : MonoBehaviour, IDragHandler, IBeginDragHandler
 {
 
     private Vector3 mousePositionOffset;
     private float mouseZCoord;
 
-   private Vector3 GetMouseWorldPosition(){
+    private Vector3 GetMouseWorldPosition(){
         Vector3 mousePosition = Input.mousePosition;
         mousePosition.z = mouseZCoord;
-        return  GameObject.Find("DM Camera").GetComponent<Camera>().WorldToScreenPoint(mousePosition);
+        return  GameObject.Find("DM Camera").GetComponent<Camera>().ScreenToWorldPoint(mousePosition);
     }
 
-    private void OnMouseDown(){
-        mouseZCoord = GameObject.Find("DM Camera").GetComponent<Camera>().WorldToScreenPoint(gameObject.transform.position).z;
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData){
+        mouseZCoord = GameObject.Find("DM Camera").GetComponent<Camera>().WorldToViewportPoint(gameObject.transform.position).z; //Causing Issues
+        //Changing the Z cord is tricky with out moving the camera to transform the Z axis into an X axis
         mousePositionOffset = gameObject.transform.position - GetMouseWorldPosition();
         Debug.Log(mousePositionOffset);
     }
-    private void OnMouseDrag(){
-        transform.position = (GetMouseWorldPosition() - mousePositionOffset)/500;
-        Debug.Log(transform.position);
+    void IDragHandler.OnDrag(PointerEventData eventData){
+        transform.position = GetMouseWorldPosition() - mousePositionOffset;
+        //Debug.Log(transform.position + ":" + GetMouseWorldPosition());
     }
+
 }
--- Assets/Scripts/DM_Scripts/DM_Menus/isSelected.cs
+++ Assets/Scripts/DM_Scripts/DM_Menus/isSelected.cs
@@ -5,10 +5,24 @@ public class isSelected : MonoBehaviour, IPointerClickHandler
 {
     public SidebarUI sidebarUI;
 
+    private float Clicked = 0;
+    private float ClickTime = 0;
+    private float ClickTimeDelay = 0.75f;
+
     public void OnPointerClick(PointerEventData eventData)
     {
-        //Debug.Log(this.gameObject + " :Is the clicked object");
-        sidebarUI.selectedObject = this.gameObject;
+        Clicked++;
+        if(Clicked == 1){
+            ClickTime = Time.time;
+        }
+        if((Clicked == 2) && (Time.time - ClickTime) < ClickTimeDelay){
+            //Debug.Log(this.gameObject + " :Is the clicked object");
+            sidebarUI.selectedObject = this.gameObject;
+            Clicked = 0;
+            ClickTime = 0;
+        }else if(Clicked > 2 || Time.time - ClickTime > 1){
+            Clicked = 0; 
+        }
     }
 
 }
