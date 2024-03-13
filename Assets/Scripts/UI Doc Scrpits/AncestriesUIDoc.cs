21d7fdea790bd9f36fe5dd5189f467ba9492a05b
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2024-03-13 18:09:03 -0500

    Got the Added stats bonus to appear
    Can't chose free attribute yet, and changing it to variably spawn dropdowns for free attribute

--- Assets/Scripts/UI Doc Scrpits/AncestriesUIDoc.cs
+++ Assets/Scripts/UI Doc Scrpits/AncestriesUIDoc.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,6 +8,7 @@ public class AncestriesUIDoc : MonoBehaviour
     public JSONDataLoader dataLoader;
     private VisualElement Ancestries;
     private VisualElement AncestrySummaries;
+    private VisualElement AncestryStatIncrease;
     private StyleSheet AncestriesButtons;
     private bool populate = true;
 
@@ -15,6 +17,7 @@ public class AncestriesUIDoc : MonoBehaviour
         root = GetComponent<UIDocument>().rootVisualElement;
         Ancestries = root.Q<VisualElement>("Ancestries").Q<VisualElement>("Ancestries-Holder");
         AncestrySummaries = root.Q<VisualElement>("AncestrySummry").Q<VisualElement>("AncestrySummry");
+        AncestryStatIncrease = root.Q<VisualElement>("AncestrySummry").Q<VisualElement>("Stat-Increase").Q<VisualElement>("Preset-Stat");
         AncestriesButtons = Resources.Load<StyleSheet>("CSS/AnceseryButtons");
 
         populateAncestries();
@@ -38,7 +41,7 @@ public class AncestriesUIDoc : MonoBehaviour
                     //NewAncestry.onClick.AddListener(delegate { populateAncestSumry(); });
                     //Debug.Log(NewAncestry);
                     NewAncestry.styleSheets.Add(AncestriesButtons);
-                    NewAncestry.AddManipulator(new Clickable(click => populateAncestSumry(NewAncestry.name)));
+                    NewAncestry.AddManipulator(new Clickable(click => populateAncestry(NewAncestry.name)));
                     Ancestries.Add(NewAncestry);
                 }
             }
@@ -46,8 +49,25 @@ public class AncestriesUIDoc : MonoBehaviour
         populate = false;
     }
 
-    private void populateAncestSumry(System.String name2){
-        Debug.Log("IM here!!!");
-        AncestrySummaries.Q<Label>("Summery").text = dataLoader.ancestryList.Find(x => x.name == name2).summary;
+    private void populateAncestry(String RaceName){
+        populateAncestSumry(RaceName);
+        populateStatIncrease(RaceName);
     }
+    private void populateAncestSumry(String RaceName){
+        //Debug.Log("IM here!!!");
+        AncestrySummaries.Q<Label>("Summery").text = dataLoader.ancestryList.Find(x => x.name == RaceName).summary;
+    }
+    private void populateStatIncrease(String RaceName){
+
+        if (dataLoader.ancestryList.Find(x => x.name == RaceName).ability[0] != "Free")
+            AncestryStatIncrease.Q<Label>("Trait-Increase-one").text = dataLoader.ancestryList.Find(x => x.name == RaceName).ability[0];
+        else
+            AncestryStatIncrease.Q<Label>("Trait-Increase-one").text = "None";
+
+        if(dataLoader.ancestryList.Find(x => x.name == RaceName).ability[1] != "Free")
+            AncestryStatIncrease.Q<Label>("Trait-Increase-two").text = dataLoader.ancestryList.Find(x => x.name == RaceName).ability[1];
+        else
+            AncestryStatIncrease.Q<Label>("Trait-Increase-two").text = "None";
+    }
+
 }
