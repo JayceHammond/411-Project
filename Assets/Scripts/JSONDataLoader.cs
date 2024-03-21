0c9e58a8efe0980ef2811962aab597854c095c60
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2024-03-21 15:58:39 -0500

    Got the Class JSON into a list
    With the help of Gabe, got the objects in the class JSON into a list for scrpiting.

--- Assets/Scripts/JSONDataLoader.cs
+++ Assets/Scripts/JSONDataLoader.cs
@@ -9,6 +9,11 @@ public class AncestryDataArrayWrapper{
     public List<JSONDataLoader.AncestryData> ancestryData;
 }
 
+[System.Serializable]
+public class ClassDataArrayWrapper{
+    public List<JSONDataLoader.ClassData> classData;
+}
+
 public class JSONDataLoader : MonoBehaviour
 {
     // Define a class to represent the structure of each ancestry entry
@@ -30,6 +35,65 @@ public class JSONDataLoader : MonoBehaviour
         public string rarity;
     }
 
+    [System.Serializable]
+    public class ClassData
+    {
+        public string name;
+        public string[] ability;
+        public AttackProficiency attack_proficiency;
+        public DefenseProficiency defense_proficiency;
+        public string fortitude_proficiency;
+        public string reflex_proficiency;
+        public string will_proficiency;
+        public string perception_proficiency;
+        public Dictionary<string, string> weakness;
+        public Dictionary<string, string> resistance;
+        public string[] skill_proficiency;
+        public string hp;
+    }
+
+    // Define a class to represent the structure of attack proficiency data
+    [System.Serializable]
+    public class AttackProficiency {
+        //Basic Weapon Groups
+        public string simple_weapons;
+        public string martial_weapons;
+        public string advanced_weapons;
+        public string unarmed_attacks;
+
+        //Weapons
+        public string alchemical_bombs;
+        public string longsword;
+        public string rapier;
+        public string sap;
+        public string shortbow;
+        public string shortsword;
+        public string whip;
+        public string club;
+        public string crossbow;
+        public string dagger;
+        public string heavy_crossbow;
+        public string staff;
+        public string advanced_firearms;
+        public string advanced_crossbows;
+        public string simple_firearms;
+        public string simple_crossbows;
+        public string martial_firearms;
+        public string martial_crossbows;
+    }
+
+    // Define a class to represent the structure of defense proficiency data
+    [System.Serializable]
+    public class DefenseProficiency{
+        public string light_armor;
+        public string medium_armor;
+        public string heavy_armor;
+        public string unarmored_defense;
+    }
+
+     // Define a class to represent the structure of skill proficiency data
+    //[System.Serializable]
+
     // Define a class to represent the structure of speed data
     [System.Serializable]
     public class SpeedData
@@ -48,8 +112,26 @@ public class JSONDataLoader : MonoBehaviour
         return new List<AncestryData>((IEnumerable<AncestryData>)dataArray.ancestryData);
     }
 
+    // Create a list to store all class data
+    public List<ClassData> classList;
+    public List<ClassData> ParseJsonArrayClass(string json){
+        // Use JsonUtility to directly deserialize the JSON array into a list of AncestryData objects
+        ClassDataArrayWrapper dataArray = JsonUtility.FromJson<ClassDataArrayWrapper>(json);
+        //Debug.Log(dataArray.ancestryData[1]);
+        return new List<ClassData>((IEnumerable<ClassData>)dataArray.classData);
+    }
+
     void Start()
     {
+        makeAncestryList();
+        makeClassList();
+
+        Debug.Log(classList[0].attack_proficiency.unarmed_attacks);
+    }
+
+
+    private void makeAncestryList(){
+        
         // Load JSON file from Resources folder
         TextAsset jsonFile = Resources.Load<TextAsset>("ancestry");
 
@@ -70,6 +152,32 @@ public class JSONDataLoader : MonoBehaviour
         {
             Debug.LogError("Ancestry JSON file not found!");
         }
+    
+    }
+
+     private void makeClassList(){
+        
+        // Load JSON file from Resources folder
+        TextAsset jsonFile = Resources.Load<TextAsset>("class");
+
+        // Parse JSON data into list of AncestryData objects
+        if (jsonFile != null)
+        {
+            classList = new List<ClassData>();
+            string jsonString = jsonFile.ToString();
+            //ancestryList.AddRange((IEnumerable<AncestryData>)JsonUtility.FromJson<AncestryData>(jsonString));
+            List<ClassData> classDataList = ParseJsonArrayClass(jsonString);
+            foreach(ClassData data in classDataList){
+                classList.Add(data);
+                //Debug.Log(JsonUtility.ToJson(data));
+            }
+            classList.Sort((x, y) => x.name.CompareTo(y.name));
+        }
+        else
+        {
+            Debug.LogError("Ancestry JSON file not found!");
+        }
+    
     }
 
 }
