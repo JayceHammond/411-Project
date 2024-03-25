43e459643d3142ab3abdb5da612c1ab710681ad5
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2024-03-25 12:26:56 -0500

    Started to fill the class stuff

--- Assets/Scripts/UI Doc Scrpits/ClassesUIDoc.cs
+++ Assets/Scripts/UI Doc Scrpits/ClassesUIDoc.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,15 @@ public class ClassesUIDoc : MonoBehaviour
     public JSONDataLoader dataLoader;
     
     private VisualElement Classes;
+    private VisualElement Charaistics;
+    private VisualElement Abilities;
+    private VisualElement Proficiencies;
+    private VisualElement Saves;
+    private VisualElement Attack_Defense;
+    private VisualElement Resistance_Weakness;
+    private VisualElement Skills;
+    private VisualElement Health;
+    private Label SelectedClassName;
     private StyleSheet AncestriesButtons;
 
     private bool populate = true; 
@@ -18,6 +28,18 @@ public class ClassesUIDoc : MonoBehaviour
     {
         root = GetComponent<UIDocument>().rootVisualElement;
         Classes = root.Q<VisualElement>("Main").Q<VisualElement>("ClassMenu").Q<VisualElement>("Classes").Q<VisualElement>("Classes-Holder");
+        SelectedClassName = root.Q<VisualElement>("Main").Q<VisualElement>("ClassMenu").Q<VisualElement>("ClassSummry").Q<Label>("ClassName");
+
+        Charaistics = root.Q<VisualElement>("Main").Q<VisualElement>("ClassMenu").Q<VisualElement>("ClassSummry").Q<VisualElement>("Chartistics-of-Class");
+        Abilities = Charaistics.Q<VisualElement>("Abilities");
+        Proficiencies = Charaistics.Q<VisualElement>("Proficiencies-Holder");
+
+        Saves = Proficiencies.Q<VisualElement>("Saves");
+        Attack_Defense = Proficiencies.Q<VisualElement>("Attack-Defense");
+        Resistance_Weakness = Proficiencies.Q<VisualElement>("Resistance-Weakness");
+        Skills = Proficiencies.Q<VisualElement>("Skill");
+        Health = Proficiencies.Q<VisualElement>("Health");
+
 
         //Asigning the CSS to use 
         AncestriesButtons = Resources.Load<StyleSheet>("CSS/AnceseryButtons");
@@ -33,7 +55,7 @@ public class ClassesUIDoc : MonoBehaviour
      private void populateClasses(){
         if (populate) //Makesure this runs once
         {
-            //Grabs all the Ancestries from the JSON file
+            //Grabs all the Classes from the JSON file
             for (int i = 0; i < dataLoader.classList.Count; i++)
             {
                 //Debug.Log(dataLoader.classList[i].name);
@@ -53,7 +75,50 @@ public class ClassesUIDoc : MonoBehaviour
     }
 
     private void populateClass(string className){
+        changeSelectedName(className); //Changes the Label at the top
+        populateSummery(className); //Changes the summery for the selected class
+        populateCharaistics(className); //Changes the charaistics for the selected class
+    }
 
+    private void changeSelectedName(String className){
+        SelectedClassName.text = dataLoader.classList.Find(x => x.name == className).name;
+    }
+
+    private void populateSummery(string className){
+        //Populates Class Summery, whenever it gets added to the JSON
+    }
+
+    //Populates the attack, defense, skill proficiency, health, and saves
+    private void populateCharaistics(string className){
+        //Get the class aspects from the JSON using the name
+        String Health = dataLoader.classList.Find(x => x.name == className).hp;
+        Proficiencies.Q<VisualElement>("Health").Q<Label>("Amount").text = Health;
+
+
+        String Fortitude = dataLoader.classList.Find(x => x.name == className).fortitude_proficiency;
+        String Reflex = dataLoader.classList.Find(x => x.name == className).reflex_proficiency;
+        String Will = dataLoader.classList.Find(x => x.name == className).will_proficiency;
+        String Perception = dataLoader.classList.Find(x => x.name == className).perception_proficiency;
+
+        Saves.Q<VisualElement>("Fortitude").Q<Label>("Proficiency").text = Fortitude;
+        Saves.Q<VisualElement>("Reflex").Q<Label>("Proficiency").text = Reflex;
+        Saves.Q<VisualElement>("Will").Q<Label>("Proficiency").text = Will;
+        Saves.Q<VisualElement>("Perception").Q<Label>("Proficiency").text = Perception;
+
+        String[] Abilities = dataLoader.classList.Find(x => x.name == className).ability;
+         = dataLoader.classList.Find(x => x.name == className).attack_proficiency;
+
+
+
+
+    }
+
+    private Color setLabelColor(String Proficiency){
+        if(Proficiency == "Trained" || Proficiency == "Expert" ){
+            return Color.green;
+        }else{
+            return Color.red;
+        }
     }
 
 }
