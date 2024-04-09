e454188ad13e637eb8a03aeb4e021e26914151b3
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2024-04-09 18:32:22 -0500

    Fixed SpriteBillboarding

--- Assets/Scripts/SpriteBillboard.cs
+++ Assets/Scripts/SpriteBillboard.cs
@@ -1,8 +1,10 @@
+using Unity.Collections;
 using UnityEngine;
 
 public class SpriteBillboard : MonoBehaviour
 {
     public Camera GameplayCamera;
+    public Quaternion SpriteQuat;
 
     void OnEnable(){
         GameplayCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
@@ -15,6 +17,13 @@ public class SpriteBillboard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = Quaternion.Euler(transform.rotation.x,GameplayCamera.transform.rotation.eulerAngles.y,transform.rotation.z);
+        SpriteQuat = new Quaternion{
+            x = transform.localRotation.x,
+            y = Quaternion.Euler(0,GameplayCamera.transform.rotation.eulerAngles.y,0).y,
+            z = transform.localRotation.z,
+            w = transform.localRotation.w,
+        };
+
+        transform.rotation = SpriteQuat;
     }
 }
