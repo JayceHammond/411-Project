88b170e40ef6434bb42e9c4863cbf4509f109ace
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2024-04-11 13:11:43 -0500

    Trees populate inside of TreeGenerator

--- Assets/Scripts/TreeGenerator.cs
+++ Assets/Scripts/TreeGenerator.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class StartGame : MonoBehaviour {
  
     public GameObject theTree;
-    public GameObject theBush;
+    //public GameObject theBush; //Can't do bushes the same way until I can grab speific list
     float offset;
     List<TreeInstance> SavedTrees;
  
@@ -19,15 +19,17 @@ public class StartGame : MonoBehaviour {
         TerrainData theGround;
         theGround = GameObject.Find("Ground").GetComponent<Terrain>().terrainData;
         Debug.Log(offset);
-        // For every tree on the island
+        // For every tree on the ground
+        Debug.Log(theGround.treeInstances);
         foreach (TreeInstance tree in theGround.treeInstances) {
             // Find its local position scaled by the terrain size (to find the real world position)
             Vector3 worldTreePos = Vector3.Scale(tree.position, theGround.size) + Terrain.activeTerrain.transform.position;
             worldTreePos.y = worldTreePos.y + offset;
-            Instantiate (theTree, worldTreePos, Quaternion.identity); // Create a prefab tree on its pos
+            var Tree = Instantiate (theTree, worldTreePos, Quaternion.identity); // Create a prefab tree on its pos
+            Tree.transform.parent = GameObject.Find("TreeGenerator").transform;
         }
         
-        // Then delete all trees on the island
+        // Then delete all trees on the ground
         List<TreeInstance> newTrees = new List<TreeInstance>(0);
         SavedTrees = new List<TreeInstance>(theGround.treeInstances.Length);
         SavedTrees = theGround.treeInstances.ToList();
@@ -35,6 +37,7 @@ public class StartGame : MonoBehaviour {
         
     }
 
+    //Adds back the trees in terrain data so, we don't have to remake every run
     void OnApplicationQuit(){
         TerrainData theGround;
         theGround = GameObject.Find("Ground").GetComponent<Terrain>().terrainData;
