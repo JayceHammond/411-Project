d750df06e85992c8bb97e3cf03a0090a45a0b144
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2024-04-16 13:06:00 -0500

    Random Tree Generation

--- Assets/Scripts/Terrain/TreeGenerator.cs
+++ Assets/Scripts/Terrain/TreeGenerator.cs
@@ -1,27 +1,30 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
  
 public class StartGame : MonoBehaviour {
  
-    public GameObject theTree;
-    //public GameObject theBush; //Can't do bushes the same way until I can grab speific list
+    public GameObject theTallTree;
+    public GameObject theMediumTree; 
     float offset;
+    List<GameObject> SpawningTrees;
     List<TreeInstance> SavedTrees;
  
     // Use this for initialization
     void Start () {
+        SpawningTrees = treeArray();
 
-        theTree = Resources.Load<GameObject>("Prefabs/Tall Tree");
-        offset = theTree.GetComponent<SpriteRenderer>().size.y/2;
         // Grab the Ground's terrain data
         TerrainData theGround;
         theGround = GameObject.Find("Ground").GetComponent<Terrain>().terrainData;
-        Debug.Log(offset);
+
         // For every tree on the ground
-        Debug.Log(theGround.treeInstances);
         foreach (TreeInstance tree in theGround.treeInstances) {
+
+            GameObject theTree = SpawningTrees[randomTreePicker(SpawningTrees)];
+
+            offset = theTree.GetComponent<SpriteRenderer>().size.y/2;
+
             // Find its local position scaled by the terrain size (to find the real world position)
             Vector3 worldTreePos = Vector3.Scale(tree.position, theGround.size) + Terrain.activeTerrain.transform.position;
             worldTreePos.y = worldTreePos.y + offset;
@@ -35,6 +38,22 @@ public class StartGame : MonoBehaviour {
         SavedTrees = theGround.treeInstances.ToList();
         theGround.treeInstances = newTrees.ToArray();
         
+    }
+
+    private List<GameObject> treeArray(){
+        List<GameObject> Trees = new List<GameObject>();
+
+        theTallTree = Resources.Load<GameObject>("Prefabs/Tall Tree");
+        theMediumTree = Resources.Load<GameObject>("Prefabs/Medium Tree");
+
+        Trees.Add(theTallTree);
+        Trees.Add(theMediumTree);
+
+        return Trees;
+    }
+
+    private int randomTreePicker(List<GameObject> Trees){
+        return Random.Range(0,Trees.Count);
     }
 
     //Adds back the trees in terrain data so, we don't have to remake every run
