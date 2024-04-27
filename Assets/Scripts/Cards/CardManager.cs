cf3845731076d3793867d67557e7a57cdb3b42a4
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2024-04-27 11:18:40 -0500

    Card broken

--- Assets/Scripts/Cards/CardManager.cs
+++ Assets/Scripts/Cards/CardManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CardManager : MonoBehaviour{
 
@@ -13,8 +14,14 @@ public class CardManager : MonoBehaviour{
     public List<Card> AttackDeck = new List<Card>();
     public List<Card> SpellDeck = new List<Card>();
 
+    private VisualElement CardOne;
+
     void Start(){
         AllCards = Resources.LoadAll<GameObject>("Cards").ToList<GameObject>();
+
+        CardOne = GameObject.Find("Card 1").GetComponent<UIDocument>().rootVisualElement;
+
+        CardOne.Q<VisualElement>("CardBase").Q<VisualElement>("InnerCard").Q<VisualElement>("TopInfo").Q<VisualElement>("SecondLayer").Q<Label>("Title").text = "FireBall";
 
     }
 
